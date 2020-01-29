041f9be5af15d5db5c9c801e763bfb273780abdb
田鲁攀 <tianlp@jiedaibao.com>
2020-01-29 10:15:01 +0800

    正确处理继承自Component的情况

--- src/transforms/import-props-states-transform.ts
+++ src/transforms/import-props-states-transform.ts
@@ -37,87 +37,91 @@ function importPropsStatesTransformFactoryFactory(typeChecker: ts.TypeChecker, c
     function visitSourceFile(sourceFile: ts.SourceFile, typeCheck: ts.TypeChecker) {
         let statements = sourceFile.statements;
         let superComponentName : string | undefined = undefined;
-        let classDeclaration: ts.Statement | undefined = undefined;
+        let classDeclaration: ts.Statement |  undefined = undefined;
         let componentName: string | undefined = undefined;
+        let customExtend = false;
         statements.find(statement=>{
-            if((ts.isClassDeclaration(statement) || ts.isClassExpression(statement)) && statement.heritageClauses && statement.heritageClauses.length==1){
-                if(statement.heritageClauses[0].types.length==1){
-                    const firstType = statement.heritageClauses[0].types[0];
-                    const extendClassType = typeChecker.getTypeAtLocation(firstType);
-                    // check extend component or subclass of component
-                    if(extendClassType && extendClassType.getProperties().find(property=> property.name==="shouldComponentUpdate")){
-                        if(!firstType.typeArguments && ts.isIdentifier(firstType.expression)) {
-                            superComponentName = firstType.expression.text;
-                            classDeclaration = statement;
-                            if(statement.name && ts.isIdentifier(statement.name)){
-                                componentName = statement.name.text;
-                            }
-                            return true;
-                        }
+            if(ts.isClassDeclaration(statement) || ts.isClassExpression(statement)) {
+                superComponentName = helpers.getReactComponentSuperClassName(statement, typeChecker);
+                if(superComponentName){
+                    classDeclaration = statement;
+                    customExtend = helpers.isReactComponentGrandson(statement, typeChecker) || false;
+                    if(statement.name && ts.isIdentifier(statement.name)){
+                        componentName = statement.name.text;
                     }
+                    return true;
                 }
+                return false;
             }
-            return false;
         });
 
         if(superComponentName){
-            statements.filter(statement=>{
-                if(ts.isImportDeclaration(statement) && statement.importClause && statement.importClause.name && ts.isIdentifier(statement.importClause.name) && statement.importClause.name.text==superComponentName){
-                    return true;
-                }
-                return  false;
-            }).forEach(statement =>{
-                const customExtend = !["React.Component","Component"].includes(superComponentName!);
-
-                const importDeclaration = statement as ts.ImportDeclaration;
-                if(customExtend && importDeclaration.importClause) {
-                    let added = [
-                        ts.createImportSpecifier(ts.createIdentifier(CLASS_NAMES.props.name), ts.createIdentifier(CLASS_NAMES.props.alias)),
-                        ts.createImportSpecifier(ts.createIdentifier(CLASS_NAMES.states.name), ts.createIdentifier(CLASS_NAMES.states.alias))
-                    ];
-                    if (importDeclaration.importClause.namedBindings && ts.isNamedImports(importDeclaration.importClause.namedBindings)) {
-                        // importDeclaration.importClause.namedBindings.element
-                        importDeclaration.importClause.namedBindings.elements.forEach(value => {
-                            added.push(value);
-                        })
+            if(customExtend) {
+                statements.filter(statement => {
+                    if (ts.isImportDeclaration(statement) && statement.importClause) {
+                        // import XXX
+                        if(statement.importClause.name && ts.isIdentifier(statement.importClause.name) && statement.importClause.name.text == superComponentName) {
+                            return true;
+                        }
+                        //import {XXX}
+                        if(statement.importClause.namedBindings && ts.isNamedImports(statement.importClause.namedBindings)){
+                            return  statement.importClause.namedBindings.elements.find(element=>{
+                                return  ts.isIdentifier(element.name) && element.name.text===superComponentName;
+                            })
+                        }
                     }
-                    const importClause = ts.updateImportClause(
-                        importDeclaration.importClause,
-                        importDeclaration.importClause.name,
-                        ts.createNamedImports(added)
-                    );
-                    const newImportDeclaration = ts.updateImportDeclaration(importDeclaration,
-                        importDeclaration.decorators,
-                        importDeclaration.modifiers,
-                        importClause,
-                        importDeclaration.moduleSpecifier);
+                    return false;
+                }).forEach(statement => {
+                    const importDeclaration = statement as ts.ImportDeclaration;
+                    if (importDeclaration.importClause) {
+                        let added = [
+                            ts.createImportSpecifier(ts.createIdentifier(CLASS_NAMES.props.name), ts.createIdentifier(CLASS_NAMES.props.alias)),
+                            ts.createImportSpecifier(ts.createIdentifier(CLASS_NAMES.states.name), ts.createIdentifier(CLASS_NAMES.states.alias))
+                        ];
+                        if (importDeclaration.importClause.namedBindings && ts.isNamedImports(importDeclaration.importClause.namedBindings)) {
+                            // importDeclaration.importClause.namedBindings.element
+                            importDeclaration.importClause.namedBindings.elements.forEach(value => {
+                                added.push(value);
+                            })
+                        }
+                        const importClause = ts.updateImportClause(
+                            importDeclaration.importClause,
+                            importDeclaration.importClause.name,
+                            ts.createNamedImports(added)
+                        );
+                        const newImportDeclaration = ts.updateImportDeclaration(importDeclaration,
+                            importDeclaration.decorators,
+                            importDeclaration.modifiers,
+                            importClause,
+                            importDeclaration.moduleSpecifier);
 
-                    statements = ts.createNodeArray(helpers.replaceItem(statements, statement, newImportDeclaration));
-                }
+                        statements = ts.createNodeArray(helpers.replaceItem(statements, statement, newImportDeclaration));
+                    }
+                });
+            }
 
-                let propsInterfaceDeclaration: ts.Statement = ts.createInterfaceDeclaration(
-                    [],
-                    [],
-                    `${componentName}${CLASS_NAMES.props.name}`,
-                    [],
-                    customExtend ? [createHeritageClause(CLASS_NAMES.props.alias)] : undefined,
-                    [],
-                );
-                let statesInterfaceDeclaration: ts.Statement = ts.createInterfaceDeclaration(
-                    [],
-                    [],
-                    `${componentName}${CLASS_NAMES.states.name}`,
-                    [],
-                    customExtend ? [createHeritageClause(CLASS_NAMES.states.alias)] : undefined,
-                    [],
-                );
+            let propsInterfaceDeclaration: ts.Statement = ts.createInterfaceDeclaration(
+                [],
+                [],
+                `${componentName}${CLASS_NAMES.props.name}`,
+                [],
+                customExtend ? [createHeritageClause(CLASS_NAMES.props.alias)] : undefined,
+                [],
+            );
+            let statesInterfaceDeclaration: ts.Statement = ts.createInterfaceDeclaration(
+                [],
+                [],
+                `${componentName}${CLASS_NAMES.states.name}`,
+                [],
+                customExtend ? [createHeritageClause(CLASS_NAMES.states.alias)] : undefined,
+                [],
+            );
 
-                const allTypeDeclarations: ts.Statement[] = [];
-                allTypeDeclarations.push(propsInterfaceDeclaration);
-                allTypeDeclarations.push(statesInterfaceDeclaration);
+            const allTypeDeclarations: ts.Statement[] = [];
+            allTypeDeclarations.push(propsInterfaceDeclaration);
+            allTypeDeclarations.push(statesInterfaceDeclaration);
 
-                statements = ts.createNodeArray(helpers.insertBefore(statements, classDeclaration!, allTypeDeclarations));
-            });
+            statements = ts.createNodeArray(helpers.insertBefore(statements, classDeclaration!, allTypeDeclarations));
         }
         return ts.updateSourceFileNode(sourceFile, statements);
     }
